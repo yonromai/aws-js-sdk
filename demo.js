08633c8188a54f7aa5812c9cfca1dbe729c442fc
Romain Yon <romain.yon@gmail.com>
2013-04-08 23:46:04 +0200

    adding some crap

--- demo.js
+++ demo.js
@@ -8,3 +8,15 @@ var AWSAccessKeyId = "AKIAIOSFODNN7EXAMPLE",
     };
 
 aws_js_sdk.s3(AWSAccessKeyId, YourSecretAccessKeyID, query_params);
+
+AWSAccessKeyId = "AKIAIOSFODNN7EXAMPLE";
+YourSecretAccessKeyID = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+query_params = {
+  http_verb: "GET",
+  date:"Tue, 27 Mar 2007 19:42:41 +0000",
+  bucket:"johnsmith",
+  ressource_path:"/"
+};
+
+aws_js_sdk.s3(AWSAccessKeyId, YourSecretAccessKeyID, query_params);
+
