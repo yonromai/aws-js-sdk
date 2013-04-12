f839ae13fb90c44cb92b9ca25dcb5e2a55a55f76
Ubuntu <ubuntu@ip-10-245-55-61.us-west-2.compute.internal>
2013-04-12 22:58:08 +0000

    cleaned server code

--- server/api_calls.js
+++ server/api_calls.js
@@ -2,51 +2,56 @@ aws_api = {
 	handle : require('aws-sdk'),
 	akid : process.env.SWS_AKID,
 	secret : process.env.SWS_SECRET,
+	token: null,
 	region : 'us-west-2',
 
 	update : function(params) {
-		this.akid = params['akid']? params['akid'] : this.akid;
- 		this.secret = params['secret']? params['secret'] : this.secret;
-		this.region = params['region']? params['region'] : this.region;
-		
-		this.handle.config.update({ accessKeyId: this.akid , secretAccessKey: this.secret });
-		this.handle.config.update({region: this.region});
+		this.akid = params.AKID ? params.AKID : this.akid;
+ 		this.secret = params.Secret ? params.Secret : this.secret;
+		this.region = params.Region ? params.Region : this.region;
+		this.token = params.Token ? params.Token : this.token;
+
+		this.handle.config.update({
+			accessKeyId: this.akid,
+			secretAccessKey: this.secret,
+			sessionToken: this.token,
+			region: this.region
+		});
 	},
-	
-	
-	addUserToGroup : function (groupName, userName, callback) {
-		this.update({region : 'us-east-1'});
+
+	addUserToGroup : function (params, callback) {
+		this.update({Region : 'us-east-1'});
 		var svc = new this.handle.IAM();
 		
-		svc.client.createGroup({GroupName: groupName}, function(err, data) {
+		svc.client.createGroup({GroupName: params.GroupName}, function(err, data) {
   		if (err) { 
-    		console.log('Failed to create group ' + groupName + ' : ' + err);
+    		console.log('Failed to create group ' + params.GroupName + ' : ' + err);
   		} else {
-    		console.log('Created group ' + groupName);
+    		console.log('Created group ' + params.GroupName);
 		  }
 			
-			if (err && err['statusCode'] != 409) {
+			if (err && err.statusCode != 409) {
 				callback(err, data);
 				return;
 			}
 			
-			svc.client.createUser({UserName: userName}, function(err, data) {
+			svc.client.createUser({UserName: params.UserName}, function(err, data) {
 				if (err) {
-						console.log('Failed to create user ' + userName + ' : ' + err);
+						console.log('Failed to create user ' + params.UserName + ' : ' + err);
 				} else {
-					console.log('Created user ' + userName);
+					console.log('Created user ' + params.UserName);
 				}
 				
-				if (err && err['statusCode'] != 409) {
+				if (err && err.statusCode != 409) {
 					callback(err, data);
 					return;
 				}
 				
-				svc.client.addUserToGroup({GroupName: groupName, UserName: userName}, function(err, data) {
+				svc.client.addUserToGroup(params, function(err, data) {
 					if (err) {
-						console.log('Failed to add user ' + userName + ' to group ' + groupName + ' : ' + err);
+						console.log('Failed to add user ' + params.UserName + ' to group ' + params.GroupName + ' : ' + err);
 					} else {
-						console.log('Successfuly added user ' + userName + ' to group ' + groupName);
+						console.log('Successfuly added user ' + params.UserName + ' to group ' + params.GroupName);
 					}
 					
 					callback(err, data);
@@ -56,8 +61,20 @@ aws_api = {
 	},
 	
 	getObject : function (params, callback) {
-		this.update({region : 'us-west-1'});
-		var s3 = new this.handle.S3();
+		this.update({Region : 'us-east-1'});
+		var s3 = new this.handle.S3({
+			accessKeyId: this.akid,
+			secretAccessKey: this.secret,
+			sessionToken: this.token
+		});
+		
+		console.log("AKID: " + this.akid);
+    console.log("Secret: " + this.secret);
+    console.log("SessionToken: " + this.token);
+		
+		console.log(s3.accessKeyId);
+		console.log(s3.client.secretAccessKey);
+		console.log(s3.client.sessionToken);
 		
 		s3.client.getObject(params, function(err, data) {
 			callback(err, data);
@@ -65,17 +82,15 @@ aws_api = {
 	},
 	
 	getClientToken : function (params, callback) {
-		this.update({region: 'us-east-1'});
+		this.update({Region: 'us-east-1'});
     var svc = new this.handle.STS();
 		var fs = require('fs');
 
-		fs.readFile(params['Policy'], 'utf8', function(err, data) {
+		fs.readFile(params.Policy, 'utf8', function(err, data) {
 			if (err) {
 				console.log('Error while reading the policy file: ' + err);
 			} else {
-				console.log(data);
-				params['Policy'] = data;
-				console.log(params);
+				params.Policy = data;
 				
 				svc.client.getFederationToken(params, function(err, data) {
 					callback(err, data);
--- server/demo_credentials.js
+++ server/demo_credentials.js
@@ -1,22 +1,19 @@
 
-if (process.argv.length < 4) {
-	// console.log('Usage: node demo_credentials.js [akid] [secret] [bucket] [file_key]');
-	console.log('Usage: node demo_credentials.js [user] [policy_file]');
+if (process.argv.length < 5) {
+	// console.log('Usage: node demo_credentials.js [user] [policy_file] [bucket] [file_key]');
+	console.log('Usage: node demo_credentials.js [user] [policy_file] [duration]');
 	process.exit();
 }
 
 require('./api_calls.js');
 
-/* var akid = process.argv[2];
-var secret = process.argv[3];
-var bucket = process.argv[4];
-var key = process.argv[5];
-*/
+var UserName = process.argv[2];
+var PolicyFile = process.argv[3];
+var Duration = process.argv[4];
+//var bucket = process.argv[4];
+//var key = process.argv[5];
 
-var userName = process.argv[2];
-var policyFile = process.argv[3];
-
-var simpleCallback = function(err, data) {
+simpleCallback = function(err, data) {
 	if (err) {
 		console.log(err);
 	} else {
@@ -25,97 +22,28 @@ var simpleCallback = function(err, data) {
 	}
 }
 
-//aws_api.update({akid: akid, secret: secret});
-// aws_api.getObject({ Bucket: bucket, Key: key }, simpleCallback);
 
-// aws_api.addUserToGroup(groupName, userName);
+// aws_api.addUserToGroup({GroupName: GroupName, UserName: UserName}, simpleCallback);
 
 var tokenCallback = function(err, data) {
-	var addUserCallback = function(err, data) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(data);
-		}
-	}
-	
 	if (err) {
+		console.log('Error getting token:');
 		console.log(err);
 	} else {
-		console.log('Success!');
-		console.log(data);
-		//var credentials = data['Credentials'];
-		//aws_api.update({ akid: credentials['SecretAccessKey'], secret: credentials['AccessKeyId'] });
-		//aws_api.addUserToGroup('hpc.group.clients', 'third_client',  addUserCallback);
+		console.log("AKID: " + data.Credentials.AccessKeyId);
+		console.log("Secret: " + data.Credentials.SecretAccessKey);
+		console.log("SessionToken: " + data.Credentials.SessionToken);
+		console.log("Expiration: " + data.Credentials.Expiration);
+		
+		/* aws_api.update({
+			AKID: data.Credentials.AccessKeyId,
+			Secret: data.Credentials.SecretAccessKey,
+			Token: data.Credentials.SessionToken
+		});
+
+		aws_api.getObject({ Bucket: bucket, Key: key }, simpleCallback); */
 	}
 }
 
-aws_api.getClientToken({Name: userName, DurationSeconds: 3600, Policy: policyFile}, tokenCallback);
+aws_api.getClientToken({Name: UserName, DurationSeconds: parseInt(Duration), Policy: PolicyFile}, tokenCallback);
 
-/* var AWS = require('aws-sdk');
-var groupName = process.argv[2];
-var userName = process.argv[3];
-
-console.log(groupName);
-console.log(userName);
-
-AWS.config.update({accessKeyId: process.env.SWS_AKID, secretAccessKey: process.env.SWS_SECRET});
-AWS.config.update({region: 'us-east-1'});
-
-var svc = new AWS.IAM();
-svc.client.createGroup({GroupName: groupName}, function(err, data) {
-	if (err) {
-		console.log('Failed to create group ' + groupName + ' : ' + err);
-		if (err['statusCode'] != 404) process.exit(err['statusCode']);
-	} else {
-		console.log('Created group ' + groupName);
-	}
-
-	svc.client.createUser({UserName: userName}, function(err, data) {
-		if (err) {
-			console.log('Failed to create user ' + userName + ' : ' + err);
-			if (err['statusCode'] != 404) process.exit(err['statusCode']);
-		} else {
-			console.log('Created user ' + userName);
-			return 0;
-		}
-	});
-});
-
-if (res == 404) {
-	res = svc.client.createUser({UserName: userName}, function(err, data) {
-		if (err) {
-			console.log('Failed to create user ' + userName + ' : ' + err);
-			return err['statusCode'];
-		} else {
-			console.log('Created user ' + userName);
-			return 0;
-		}
-	});
-}
-
-if (res != 0) process.exit(res);
-
-res = svc.client.addUserToGroup({GroupName: groupName, UserName: userName}, function(err, data) {
-	if (err) {
-		console.log('Failed to add user ' + userName + ' to group ' + groupName + ' : ' + err);
-		return err['statusCode'];
-	} else {
-		console.log('Successfuly added user ' + userName + ' to group ' + groupName);
-		return 0;
-	}
-});
-
-process.exit(res); */
-
-/* AWS.config.update({region: 'us-west-2'});
-
-var s3 = new AWS.S3();
-s3.client.deleteBucket({Bucket: 'hpc.bucket.test'}, function(err, data) {
-	console.log('S3:');
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(data);
-	}
-}); */
