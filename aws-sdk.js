9272364d9870a034fbb4d4954e10dd8d0fbae774
Romain Yon <romain.yon@gmail.com>
2013-04-12 17:08:40 +0200

    hoovering

--- aws-sdk.js
+++ aws-sdk.js
@@ -1,27 +1,11 @@
-// Authorization = "AWS" + " " + AWSAccessKeyId + ":" + Signature;
 
-// Signature = Base64( HMAC-SHA1( YourSecretAccessKeyID, UTF-8-Encoding-Of( StringToSign ) ) );
-
-// StringToSign = HTTP-Verb + "\n" +
-//   Content-MD5 + "\n" +
-//   Content-Type + "\n" +
-//   Date + "\n" +
-//   CanonicalizedAmzHeaders +
-//   CanonicalizedResource;
-
-// CanonicalizedResource = [ "/" + Bucket ] +
-//   <HTTP-Request-URI, from the protocol name up to the query string> +
-//   [ sub-resource, if present. For example "?acl", "?location", "?logging", or "?torrent"];
-
-// CanonicalizedAmzHeaders = <described below>
-
-var get_http_headers = function(AWSAccessKeyId, YourSecretAccessKeyID, token, bucket, path){
+var get_http_headers = function(AWSAccessKeyId, YourSecretAccessKeyID, tokenAmzHeader, bucket, path){
   var headers = {};
   headers.Date = (new Date()).toUTCString();
   headers.Authorization = get_authorization_header(AWSAccessKeyId, YourSecretAccessKeyID, {
     http_verb: "GET",
     date: headers.Date,
-		token: token,
+		amzHeaders: [(tokenAmzHeader ? "x-amz-security-token:" + tokenAmzHeader["x-amz-security-token"] : null)],
     bucket: bucket,
     ressource_path: path
   });
@@ -29,11 +13,33 @@ var get_http_headers = function(AWSAccessKeyId, YourSecretAccessKeyID, token, bu
 };
 
 var get_string_to_sign =  function(params){ // to refactor
-  var str = params.http_verb + "\n\n\n" + "\nx-amz-date:" + params.date;
-	if (params.token) {
-		str += '\nx-amz-security-token:' + params.token;
-	}
-	return str + '\n' + (params.bucket ? "/" + params.bucket : "") + params.ressource_path;
+  // Building the CanonicalizedResource
+  var CanonicalizedResource = "";
+  if(params.bucket)
+    CanonicalizedResource += "/" + params.bucket;
+  if(params.ressource_path)
+    CanonicalizedResource += params.ressource_path;
+  // To be compoleted, especially with the url encoded queries
+
+  // Building the CanonicalizedAmzHeaders
+  var CanonicalizedAmzHeaders = "";
+  // amzHeaders should be an array of header:value strings (or as a CSList)
+  if(params.amzHeaders && params.amzHeaders.lenght > 0){
+    params.amzHeaders.sort();
+    for(var header in params.amzHeaders)
+      CanonicalizedAmzHeaders += header + "\n";
+  }
+
+  // Building the man string to sign string
+  var string_to_sign = "";
+  string_to_sign += params.http_verb + "\n";
+  string_to_sign += (params.content ? CryptoJS.MD5(params.content): "") + "\n";
+  string_to_sign += params.content_type + "\n";
+  string_to_sign += params.date + '\n';
+  string_to_sign += CanonicalizedResource;
+  string_to_sign += CanonicalizedAmzHeaders;
+
+  return string_to_sign;
 };
 
 var get_authorization_header = function(AWSAccessKeyId, YourSecretAccessKeyID, query_params){
@@ -45,13 +51,15 @@ var get_authorization_header = function(AWSAccessKeyId, YourSecretAccessKeyID, q
   return "AWS" + " " + AWSAccessKeyId + ":" + signature;
 };
 
-var get_file = function(AWSAccessKeyId, YourSecretAccessKeyID, token, bucket, path, callback){
-  var headers = get_http_headers(AWSAccessKeyId, YourSecretAccessKeyID, token, bucket, path);
+var get_file = function(AWSAccessKeyId, YourSecretAccessKeyID, amzHeaders, bucket, path, callback){
+  var headers = get_http_headers(AWSAccessKeyId, YourSecretAccessKeyID, amzHeaders, bucket, path);
   $.ajax({
     url: "https://s3.amazonaws.com/" + bucket + path,
     beforeSend: function(xhr){
         xhr.setRequestHeader("x-amz-date", headers.Date);
         xhr.setRequestHeader("Authorization", headers.Authorization);
+        if(amzHeaders)
+          xhr.setRequestHeader("x-amz-security-token", amzHeaders["x-amz-security-token"]);
     }
   }).done(callback).fail(function(jqXHR, textStatus, errorThrown){
     console.log("FAIL:");
@@ -60,14 +68,14 @@ var get_file = function(AWSAccessKeyId, YourSecretAccessKeyID, token, bucket, pa
 };
 
 aws_js_sdk = {
-  s3: function(AWSAccessKeyId, YourSecretAccessKeyID, token){
+  s3: function(AWSAccessKeyId, YourSecretAccessKeyID, tokenAmzHeader){
     this.AWSAccessKeyId = AWSAccessKeyId;
     this.YourSecretAccessKeyID = YourSecretAccessKeyID;
-		this.token = token
+		this.tokenAmzHeader = tokenAmzHeader
     //members
-    this.get_http_headers = function(bucket, path){ return get_http_headers(this.AWSAccessKeyId, this.YourSecretAccessKeyID, this.token, bucket, path);};
+    this.get_http_headers = function(bucket, path){ return get_http_headers(this.AWSAccessKeyId, this.YourSecretAccessKeyID, this.tokenAmzHeader, bucket, path);};
     this.get_authorization_header = function(query_params) { return get_authorization_header(this.AWSAccessKeyId, this.YourSecretAccessKeyID, query_params);};
     this.get_string_to_sign = function(params) { return get_string_to_sign(params);};
-    this.get = function(bucket, path, callback) {return get_file(this.AWSAccessKeyId, this.YourSecretAccessKeyID, this.token, bucket, path, callback)};
+    this.get = function(bucket, path, callback) {return get_file(this.AWSAccessKeyId, this.YourSecretAccessKeyID, this.tokenAmzHeader, bucket, path, callback)};
   }
 };
--- demo.js
+++ demo.js
@@ -30,9 +30,9 @@ query_params = {
 
 // console.log(my_s3.get_http_headers(bucket, path));
 
-var tmpAWSAccessKeyId = "AKIAIJ7G277HYO3TFTHQ"; // "ASIAIMI36F53QZGB6QRA";
-var tmpYourSecretAccessKeyID = "q2Cdub7sFjLMqwJIeYwSOzzwu83/5UyXtbvpoBte" ; // "KJbt6XpPe01FZWPZL0wXhlV9hXuSKrtR0XJglydP";
-var token = "AQoDYXdzEI///////////wEaoAJnM+J/s24XzZ8Lx4pjHbQaQQl+lXzYnWbkrjYu9Ty4kq9tRc4Ab4+GCAspJjKwmPu/bvgeZr0EQWrDUM5hhJd67ah5X32c9/qFbAd3smWJEA0dTajhrCBR/mYRknFr8qsvVy8W+BqmvZPFB/WGJy6rwDvtc/fEkduG7zC5mq5bZ9EbpxK7i+q03vBmgQw29ACSsYCSz7gEdZLbgYkbia0fqSND87vm0RPnQt+qo0/yeNZXDxhDTchnYMdh3gVUMCb82Cr/mdoArxm/ncpOcOweOrk23Zw02doJ/12S/s41zJqbrkHggXpIkSYccl0/h2BAANqUqw+f8R7N0R+kftjPGtWoX2wU4gZYap6biLrQuesRbfIoobEVg1O7LyvLIU4g052giwU=";
+var tmpAWSAccessKeyId = "accesskey";
+var tmpYourSecretAccessKeyID = "secretkey";
+var tokenAmzHeader = {"x-amz-security-token" : "AQoDYXdzEI///////////wEaoAJnM+J/s24XzZ8Lx4pjHbQaQQl+lXzYnWbkrjYu9Ty4kq9tRc4Ab4+GCAspJjKwmPu/bvgeZr0EQWrDUM5hhJd67ah5X32c9/qFbAd3smWJEA0dTajhrCBR/mYRknFr8qsvVy8W+BqmvZPFB/WGJy6rwDvtc/fEkduG7zC5mq5bZ9EbpxK7i+q03vBmgQw29ACSsYCSz7gEdZLbgYkbia0fqSND87vm0RPnQt+qo0/yeNZXDxhDTchnYMdh3gVUMCb82Cr/mdoArxm/ncpOcOweOrk23Zw02doJ/12S/s41zJqbrkHggXpIkSYccl0/h2BAANqUqw+f8R7N0R+kftjPGtWoX2wU4gZYap6biLrQuesRbfIoobEVg1O7LyvLIU4g052giwU="};
 var bucket = "hpc.bucket.demo";
 var path = "/demo/demo_file";
 
