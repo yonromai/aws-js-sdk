06dcd0aee59fe40adfa346d04b10c955237bcf13
Romain Yon <romain.yon@gmail.com>
2013-04-25 11:13:55 +0200

    push that probably breaks everything

--- demo.js
+++ demo.js
@@ -30,13 +30,13 @@ query_params = {
 
 // console.log(my_s3.get_http_headers(bucket, path));
 
-var tmpAWSAccessKeyId = "accesskey";
-var tmpYourSecretAccessKeyID = "secretkey";
+var tmpAWSAccessKeyId = "AKIAJQRF7XEWAQTLQ3LQ";
+var tmpYourSecretAccessKeyID = "/GIL0+1FcmnKU6OtvKI4oYwUXLMwJ4sCknPmofEX";
 var tokenAmzHeader = {"x-amz-security-token" : "AQoDYXdzEI///////////wEaoAJnM+J/s24XzZ8Lx4pjHbQaQQl+lXzYnWbkrjYu9Ty4kq9tRc4Ab4+GCAspJjKwmPu/bvgeZr0EQWrDUM5hhJd67ah5X32c9/qFbAd3smWJEA0dTajhrCBR/mYRknFr8qsvVy8W+BqmvZPFB/WGJy6rwDvtc/fEkduG7zC5mq5bZ9EbpxK7i+q03vBmgQw29ACSsYCSz7gEdZLbgYkbia0fqSND87vm0RPnQt+qo0/yeNZXDxhDTchnYMdh3gVUMCb82Cr/mdoArxm/ncpOcOweOrk23Zw02doJ/12S/s41zJqbrkHggXpIkSYccl0/h2BAANqUqw+f8R7N0R+kftjPGtWoX2wU4gZYap6biLrQuesRbfIoobEVg1O7LyvLIU4g052giwU="};
 var bucket = "hpc.bucket.demo";
 var path = "/demo/demo_file";
 
-my_s3 = new aws_js_sdk.s3(tmpAWSAccessKeyId, tmpYourSecretAccessKeyID, null);
+my_s3 = new aws_js_sdk.s3(tmpAWSAccessKeyId, tmpYourSecretAccessKeyID, tokenAmzHeader);
 my_s3.get(bucket, path, function(data, textStatus, jqXHR) { 
   console.log(data);
 });
