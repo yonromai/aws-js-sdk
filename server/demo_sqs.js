5f5fb7e5a9e138ccd4bb5b5317b5a5d89d6c214e
YBadiss <yacine.badiss+git@gmail.com>
2013-04-25 15:19:38 +0200

    demo sqs

--- server/demo_sqs.js
+++ server/demo_sqs.js
@@ -10,31 +10,31 @@ require('./api_calls.js');
 var userName = process.argv[2];
 queue = process.argv[3];
 
-var popCallback = function(err, data) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log('Success!');
-		console.log(data);
-	}
-}
-
-var pushCallback = function(err, data) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log('Success!');
-		console.log(data);
-		aws_api.popMessage({ QueueUrl: queue, MaxNumberOfMessages: 1 }, popCallback);
-	}
-}
-
 var tokenCallback = function(err, data) {
+	var pushCallback = function(err, data) {
+		var popCallback = function(err, data) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log('Success Pop!');
+				console.log(data);
+			}
+		}
+
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Success Push!');
+			console.log(data);
+			aws_api.popMessage({ QueueUrl: queue, MaxNumberOfMessages: 1 }, popCallback);
+		}
+	}
+
 	if (err) {
 		console.log(err);
 	} else {
-		console.log('Success!');
-		console.log(data);
+		console.log('Success Policy!');
+		// console.log(data);
 	
 		aws_api.update(data['Credentials']);
 		aws_api.pushMessage({ QueueUrl: queue, MessageBody: "Test Message" }, pushCallback);
@@ -42,6 +42,6 @@ var tokenCallback = function(err, data) {
 }
 
 aws_api.getPolicy({Policy: queue}, function(params) {
-	console.log(params);
+	// console.log(params);
 	aws_api.getClientToken({Name: userName, DurationSeconds: 3600, Policy: params['Policy']}, tokenCallback);
 });
