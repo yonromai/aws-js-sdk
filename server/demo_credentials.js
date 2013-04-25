1fdc248cf7df0190882421ffe2218c27bfb1eb37
root <root@ip-10-245-55-61.us-west-2.compute.internal>
2013-04-25 11:34:07 +0000

    api server

--- server/demo_credentials.js
+++ server/demo_credentials.js
@@ -35,12 +35,14 @@ var tokenCallback = function(err, data) {
 	} else {
 		console.log('Success!');
 		console.log(data);
-
+	
 		aws_api.update(data['Credentials']);
-		// aws_api.getObject({ Bucket: 'hpc.bucket.demo', Key: 'demo/demo_file', Range: 'bytes=3-6' }, simpleCallback);
-		var buff = new Buffer(10);
-		buff.fill("BOUYAH");
-		aws_api.putObject({ Bucket: 'hpc.bucket.demo', Key: 'demo/demo_test', Body: buff}, putCallback);
+		
+		var buff = new Buffer("BIM ALLER", 'utf8');
+		aws_api.putObject({ Bucket: 'hpc.bucket.demo', Key: 'demo/demo_last', Body: buff}, putCallback);
+
+		aws_api.getObject({ Bucket: 'hpc.bucket.demo', Key: 'demo/demo_test' }, getCallback);
+		aws_api.getObject({ Bucket: 'hpc.bucket.demo', Key: 'demo/demo_file', Range: 'bytes=3-6' }, getCallback);
 	}
 }
 
